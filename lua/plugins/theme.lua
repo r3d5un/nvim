594fb4d94040a2c1bb3e29f614181772fe052309
r3d5un <r3d5un@pm.me>
2023-07-16 19:46:08 +0200

    Set theme to onedark_dark

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -2,6 +2,6 @@ return {
   "olimorris/onedarkpro.nvim",
   priority = 1000, -- Ensure it loads first
   config = function()
-    vim.cmd("colorscheme onedark_vivid")
+    vim.cmd("colorscheme onedark_dark")
   end,
 }
