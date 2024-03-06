8664568ba876a622399f3834b206e13e465c10c0
Øyvind Kristiansen <r3d5un@pm.me>
2024-03-06 23:33:53 +0100

    Reword comment

--- init.lua
+++ init.lua
@@ -59,7 +59,7 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
--- TIP: Disable arrow keys in normal mode
+-- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move"<CR>')
