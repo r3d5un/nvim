b8536184ed9dc6881fe2e6f402dd05eb3e03f59e
Øyvind Kristiansen <r3d5un@pm.me>
2024-03-07 21:50:05 +0100

    Add treesitter plugin

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,50 @@
+return { -- Highlight, edit, and navigate code
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"lua",
+				"markdown",
+				"vim",
+				"vimdoc",
+				"go",
+				"rust",
+				"python",
+				"json",
+				"sql",
+				"yaml",
+				"cpp",
+				"javascript",
+				"typescript",
+				"latex",
+				"r",
+				"dockerfile",
+				"css",
+				"markdown_inline",
+				"regex",
+				"query",
+				"http",
+				"c_sharp",
+				"java",
+				"toml",
+			},
+			-- Autoinstall languages that are not installed
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+
+		-- There are additional nvim-treesitter modules that you can use to interact
+		-- with nvim-treesitter. You should go explore a few and see what interests you:
+		--
+		--    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+		--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+		--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+	end,
+}
