44c7ebb776c58e88fcc291cb4b5864ebd8c50bdc
r3d5un <r3d5un@pm.me>
2024-07-11 06:17:53 +0200

    Remove outdated configuration options from the Trouble plugin config

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,9 +1,6 @@
 return {
 	"folke/trouble.nvim",
 	opts = { -- settings without a patched font or icons
-		icons = false,
-		fold_open = "v", -- icon used for open folds
-		fold_closed = ">", -- icon used for closed folds
 		indent_lines = false, -- add an indent guide below the fold icons
 		signs = {
 			-- icons / text used for a diagnostic
