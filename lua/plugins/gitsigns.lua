cfdcc1a68f096f353b2cd8056aceec04b240492a
r3d5un <r3d5un@pm.me>
2024-07-13 23:43:41 +0200

    Update gitsigns plugin configuration

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,11 +2,24 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
 	"lewis6991/gitsigns.nvim",
 	opts = {
 		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
+			add = { text = "┃" },
+			change = { text = "┃" },
 			delete = { text = "_" },
 			topdelete = { text = "‾" },
 			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		signs_staged = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		current_line_blame = true,
+		current_line_blame_opts = {
+			delay = 0,
 		},
 	},
 }
